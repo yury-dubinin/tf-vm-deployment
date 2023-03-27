15c272471c7d133d54e6d2d769db8e19587b71aa
yury-dubinin <yury.dubinin@gmail.com>
2023-03-27 17:50:19 +0200

    use B2s

--- main.tf
+++ main.tf
@@ -105,7 +105,7 @@ resource "azurerm_windows_virtual_machine" "main" {
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
-  size                  = "Standard_DS1_v2"
+  size                  = "Standard_B2s"
 
   os_disk {
     name                 = "myOsDisk"
