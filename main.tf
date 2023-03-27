8f818e2fbf5deeb1ff17c7f1876ad7b7692b9683
yury-dubinin <yury.dubinin@gmail.com>
2023-03-27 18:42:42 +0200

    added connection

--- main.tf
+++ main.tf
@@ -120,6 +120,19 @@ resource "azurerm_windows_virtual_machine" "main" {
     version   = "latest"
   }
 
+  os_profile_windows_config {
+    provision_vm_agent = true
+  }
+
+  connection {
+    type        = "winrm"
+    user        = "azureadmin"
+    password    = "Azureadmin1."
+    timeout     = "1m"
+    https       = true
+    insecure    = true
+  }
+
   provisioner "file" {
     source      = "install-python.ps1"
     destination = "C:/temp/install-python.ps1"
