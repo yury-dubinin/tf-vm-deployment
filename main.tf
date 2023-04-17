19174d84c1f948880cc711ec978855e329d3de68
yury-dubinin <yury.dubinin@gmail.com>
2023-04-17 10:24:56 +0200

    added Linux VM

--- main.tf
+++ main.tf
@@ -97,9 +97,9 @@ resource "azurerm_storage_account" "my_storage_account" {
 }
 
 
-# Create virtual machine
+# Create Windows virtual machine
 resource "azurerm_windows_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
+  name                  = "${var.prefix}-WindowsVM"
   admin_username        = "azureadmin"
   admin_password        = "azureadminA1."
   location              = azurerm_resource_group.rg.location
@@ -118,6 +118,46 @@ resource "azurerm_windows_virtual_machine" "main" {
     offer     = "WindowsServer"
     sku       = "2022-datacenter-azure-edition"
     version   = "latest"
+  }
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.my_storage_account.primary_blob_endpoint
+  }
+}
+
+resource "tls_private_key" "example_ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Create Ubuntu virtual machine
+resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
+  name                  = "${var.prefix}-UbuntuVM"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
+  size                  = "Standard_DS1_v2"
+
+  os_disk {
+    name                 = "myOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  computer_name                   = "UbuntuVM"
+  admin_username                  = "azureuser"
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = tls_private_key.example_ssh.public_key_openssh
   }
 
   boot_diagnostics {
--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,8 @@ output "admin_password" {
   sensitive = true
   value     = azurerm_windows_virtual_machine.main.admin_password
 }
+
+output "tls_private_key" {
+  value     = tls_private_key.example_ssh.private_key_pem
+  sensitive = true
+}
