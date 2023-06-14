00aeb1e82f69f3881f7b4eb53b6558f7c2df0c34
yakuramori <62520712+yury-dubinin@users.noreply.github.com>
2023-06-14 18:37:03 +0200

    Update agent.sh

--- agent.sh
+++ agent.sh
@@ -10,25 +10,27 @@ set -x
 
 exec &>> /var/log/postinstall.log
 echo "Script started at $(date +%F_%H-%M-%S)"
-sudo dnf install -y git 
+echo "Install compat-openssl10 libaio"
 sudo dnf install -y compat-openssl10 libaio
-
+echo "Install git"
+sudo yum install -y git 
 git -v
-
+echo "Install yum-utils"
 sudo yum install -y yum-utils
+echo "Install terraform"
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
-
 terraform -v
-
+echo "Download Azure DevOps Agent"
 wget -q https://vstsagentpackage.azureedge.net/agent/2.214.2/vsts-agent-rhel.6-x64-2.214.2.tar.gz
 tar zxvf ~/vsts-agent-rhel.6-x64-2.214.2.tar.gz
-
 ls -all
+echo "Install dependencies for Azure DevOps Agent"
+sudo ~/bin/installdependencies.sh
+echo "Install Config for Azure DevOps Agent"
+~/config.cmd --unattended --acceptTeeEula --agent 'ADO-AGENT-RHEL8' --pool 'AZR-NN-XAAS-POC' --replace --url "https://dev.azure.com/$ADO_ORG" --auth 'PAT' --token "$ADO_TOKEN"
 
-sudo ./bin/installdependencies.sh
-.\config.cmd --unattended --acceptTeeEula --agent 'ADO-AGENT-RHEL8' --pool 'AZR-NN-XAAS-POC' --replace --url "https://dev.azure.com/$ADO_ORG" --auth 'PAT' --token "$ADO_TOKEN"
-
-sudo ./svc.sh install
-sudo ./svc.sh start
-sudo ./svc.sh status
+sudo ~/svc.sh install
+sudo ~/svc.sh start
+sudo ~/svc.sh status
+echo "Script ended at $(date +%F_%H-%M-%S)"
